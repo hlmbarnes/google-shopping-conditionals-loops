3458b7799af207f1d24cc30ef28a59a7663814eb
Hannah Barnes <hlmbarnes@me.com>
2016-03-23 23:18:50 -0700

    updated all answers 1-6 in google-shopping.js

--- google_shopping.js
+++ google_shopping.js
@@ -2,4 +2,58 @@ var data = require("./products.json")
 
 // Write your solutions below
 
-console.log(data["items"]);
+// QUESTION 1
+// var count =0;
+// for(var i = 0; i <data['items'].length; i++){
+// 	if (data['items'][i].kind === 'shopping#product') {
+// 		count++;
+// 	}	
+// }
+// console.log(count);
+
+// imformation also stored in "currentItemCount"
+
+// QUESTION 2
+// var availability = 0
+// for(var i = 0; i <data['items'].length; i++){
+// 	// var a = 0;
+// 	// for (var a = 0; a <data['items'].product.inventories.length; a++) {
+// 		if (data['items'][i].product.inventories[0].availability === 'backorder') {
+// 			availability++;
+// 		console.log(data['items'][i])
+// 		}
+// 	}
+
+// QUESTION 3
+// var link = 0;
+// for (i = 0; i <data['items'].length; i++) {
+// 		var l= (data['items'][i].product.images.length);
+// 		if (l >1) {
+// 			console.log(data['items'][i])
+// 		}
+// }
+// QUESTION 4
+// var make = 0;
+// for (i=0; i<data['items'].length; i++){
+// 	if (data['items'][i].product.brand === 'Canon') {
+// 		console.log(data['items'][i]);
+// 	}
+// }
+
+// QUESTION 5
+// var both = 0;
+// for (i=0; i<data['items'].length; i++) {
+// 	var c = (data['items'][i].product.brand === 'Canon')
+// 	if (c && (data['items'][i].product.author['name'] === 'eBay')) {
+// 		console.log(data['items'][i]);
+// 	}
+// }
+
+// QUESTION 6
+// var three_values = 0
+// for(var i = 0; i <data['items'].length; i++){
+// 	console.log('price: '+ data['items'][i].product.inventories[0]['price'] + ' ' + 'brand: '+ data['items'][i].product.brand + ' ' + 'link: ' +data['items'][i].product.link)
+
+// }
+
+	
